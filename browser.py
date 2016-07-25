2dad1b75bab853c2afd56a9586c6112220bc482a
Mark Servilla <mark.servilla@gmail.com>
2016-07-24 21:33:16 -0600

    Add route logic for displaying revisions.

--- browser.py
+++ browser.py
@@ -19,6 +19,7 @@ from flask import render_template
 from flask import url_for
 from scopes import Scopes
 from identifiers import Identifiers
+from revisions import Revisions
 
 # logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
 #                     datefmt='%Y-%m-%d% H:%M:%S%z')
@@ -33,15 +34,25 @@ def index():
     scopes_url = url_for('scopes')
     return render_template('index.html', scopes_url=scopes_url)
 
+
 @app.route('/scopes')
 def scopes():
     scopes = Scopes().get_scopes()
     return render_template('scopes.html', scopes=scopes)
 
+
 @app.route('/identifiers/<scope>')
 def identifiers(scope):
     identifiers = Identifiers(scope=scope).get_identifiers()
-    return render_template('identifiers.html', scope=scope, identifiers=identifiers)
+    return render_template('identifiers.html', scope=scope,
+                           identifiers=identifiers)
+
+
+@app.route('/revisions/<scope>/<identifier>')
+def revisions(scope, identifier):
+    revisions = Revisions(scope=scope, identifier=identifier).get_revisions()
+    return render_template('revisions.html', scope=scope, identifier=identifier,
+                           revisions=revisions)
 
 
 if __name__ == '__main__':
